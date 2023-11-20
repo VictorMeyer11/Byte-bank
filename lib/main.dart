8451f6f587bcb1c1f9f397a5a725168021461b81
VictorMeyer11 <viniciusmello201@gmail.com>
2023-11-20 13:59:35 -0300

    First screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    body: TransactionsList(),
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Icon(Icons.add),
+    ),
+  ),
+));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TransactionsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionItem(Transaction(100.0, 1000)),
+        TransactionItem(Transaction(200.0, 2000)),
+        TransactionItem(Transaction(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class TransactionItem extends StatelessWidget {
+
+  final Transaction _transferencia;
+
+  TransactionItem(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
       )
     );
   }
+}
+
+class Transaction {
+  final double valor;
+  final int numeroConta;
+
+  Transaction(this.valor, this.numeroConta);
 }
