57c7c0fd86a01d35e2b026230f7359357508fbd3
VictorMeyer11 <viniciusmello201@gmail.com>
2023-11-21 15:22:57 -0300

    Transaction form and refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,96 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: TransactionsList(),
-    appBar: AppBar(
-      title: Text('Transferências'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+void main() => runApp(BytebankApp());
 
-class TransactionsList extends StatelessWidget {
+class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransactionItem(Transaction(100.0, 1000)),
-        TransactionItem(Transaction(200.0, 2000)),
-        TransactionItem(Transaction(300.0, 3000)),
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: TransactionForm()
+      )
+    );
+  }
+}
+
+class TransactionForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando transferências')),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0
+              ),
+              decoration: InputDecoration(
+                labelText: "Número da conta",
+                hintText: "0000"
+              ),
+              keyboardType: TextInputType.number
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: "Valor",
+                hintText: "0.00"
+              ),
+              keyboardType: TextInputType.number
+            )
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Confirmar")
+          )
+        ]
+      )
+    );
+  }
+}
+
+class TransactionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências')
+      ),
+      body: Column(
+        children: <Widget>[
+          TransactionItem(Transaction(100.0, 1000)),
+          TransactionItem(Transaction(200.0, 2000)),
+          TransactionItem(Transaction(300.0, 3000)),
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      )
     );
   }
 }
 
 class TransactionItem extends StatelessWidget {
+  final Transaction _transaction;
 
-  final Transaction _transferencia;
-
-  TransactionItem(this._transferencia);
+  TransactionItem(this._transaction);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(_transferencia.valor.toString()),
-        subtitle: Text(_transferencia.numeroConta.toString()),
+        title: Text(_transaction.valor.toString()),
+        subtitle: Text(_transaction.numeroConta.toString()),
       )
     );
   }
